9a0550ae8a393dc1fb5a97b5ccc5319b5e78ef63
Jing Liu <jingl.liu@mail.utoronto.ca>
2023-04-05 11:40:10 -0400

    implement args

--- Simulator.java
+++ Simulator.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 
 public class Simulator {
     public static void main(String[] args) throws IOException {
-        int num = 2;
-        double coverage = 1;
-        String type = "dna"; //"dna" or "aa"
-        int replicateNum = 1;
-        String mutagenesis_scheme = "NNB"; //"NNK" or "NNB"
+        int num = Integer.parseInt(args[0]);
+        String type = args[1]; //"dna" or "aa"
+        String mutagenesis_scheme = args[2]; //"NNK" or "NNB"
+        double coverage = Integer.parseInt(args[3]);
+        int replicateNum = Integer.parseInt(args[4]);
+
         GeneratorController sim = new GeneratorController(num, coverage, type, replicateNum, mutagenesis_scheme);
         sim.generate();
     }
